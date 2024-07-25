90f423d8edd57543aea6b9c555ae033b84f1a76a
Saad Elbeleidy <sbeleidy90@gmail.com>
2024-07-25 11:20:22 -0500

    update behavior api endpoint names and structure

--- story_gen/behavior_api.py
+++ story_gen/behavior_api.py
@@ -1,33 +1,51 @@
 
 import json
 from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from behavior_parser import sentence_parser
-from .. import behavior_gen
+from behavior_gen import SarBehaviorGenerator, State
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/storyParser', methods = ['POST'])
-def story_parser():
+@app.route('/get-intents', methods = ['POST'])
+@cross_origin()
+def get_intents_from_text():
+    """Parse text and break it up into sentences with attached intent and emotions
+
+    Returns:
+        Array of:   sentences with intent and emotion
+            intent: the intent for the sentence
+            content: the content of the sentence
+            emotion: the emotion to express when verbalizing the sentence
+    """
     data = json.loads(request.data)
-    res = sentence_parser(data["Story"]) 
+    res = sentence_parser(data["text"]) 
     return jsonify(res)
 
-@app.route('/storyBehaviors', methods = ['POST'])
-def story_parser():
+@app.route('/get-plan', methods = ['POST'])
+@cross_origin()
+def get_plan_from_text():
     data = json.loads(request.data)
-    #TODO: Instantiate this just once, not on every call
-    sbg = behavior_gen.SarBehaviorGenerator(None)
-    state1 = behavior_gen.State("interactive")
+    sentences = sentence_parser(data["text"]) 
+
+    sbg = SarBehaviorGenerator(None)
+    state1 = State("interactive")
     state1.add(["gaze","center"])
     state1.add(["rapport","low"])
     state1.add(["affect","neutral"])
-    #TODO: set goal affects?
-    intents = [[x["intent"], x["content"], x["emotion"]] for x in data]
+
+    intents = [[x["intent"], x["content"], x["emotion"]] for x in sentences]
     plan = sbg.planBehaviorFor(intents, state1)
-    return jsonify(plan)
+    return jsonify({
+        "sentences": sentences,
+        "plan": plan
+    })
 
 
 if __name__ == '__main__':
